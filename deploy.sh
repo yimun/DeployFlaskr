5e318f11a29165bb7945aa8977482ec8413f11ba
linwei <yimulinwei@gmail.com>
2015-09-27 22:36:04 +0800

    test4

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
+#!/bin/sh 
 
-echo $AWS
 echo "Deploy begin.."
-ssh -i /home/travis/.ssh/yimu.pem -o StrictHostKeyChecking=no -p 22 ubuntu@54.219.133.113 "cd /home ; ls"
+ssh -i ~/.ssh/yimu.pem -o StrictHostKeyChecking=no -p 80 ubuntu@54.219.133.113 "cd /home ; ls"
 echo 'done'
